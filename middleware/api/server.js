4cccbd158fde02d53340e41d23be0b59470c5dba
Ankit Tharwani <ankittharwani@gmail.com>
2016-08-22 11:09:31 +0100

    api port change

--- middleware/api/server.js
+++ middleware/api/server.js
@@ -251,6 +251,6 @@ app.get('/getHistogram', function(req, res) {
     });
   });
 
-var server = app.listen(3000, function() {
+var server = app.listen(8080, function() {
   console.log('Listening on port %d', server.address().port);
 });
